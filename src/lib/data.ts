e3a765c8364632f09ca9f0c8122b9c93bce7cc06
Kenny Tapia <kennyta20@outlook.com>
2025-05-16 11:33:19 -0600

    ch

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -103,7 +103,7 @@ export const projects = [
   },
   {
     title: "Rusty Bargain ML",
-    github: "https://github.com/kennyta20/Sweet-Lift-Taxi-ML",
+    github: "https://github.com/kennyta20/Rusty-Bargain_ML",
     image: "kennyta20/kennyta20.github.io/public/Proyecto 3.jpg",
     description: [
       "Rusty Bargain used car sales service is developing an app to attract new customers. Thanks to this app, you can quickly find out the market value of your car. We have access to the history: technical specifications, equipment versions and prices. You have to create a model that will determine the market value.",
