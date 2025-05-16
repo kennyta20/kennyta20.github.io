b9257a43ed6ec64606fe2a1e2d4989d1a79645d6
Kenny Tapia <kennyta20@outlook.com>
2025-05-15 20:01:38 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+site: 'https://kennyta20.github.io',
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
