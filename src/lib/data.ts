34b513514e5512beba74389e1fc9247037695859
Kenny Tapia <kennyta20@outlook.com>
2025-05-16 01:59:21 -0600

    :)

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -78,8 +78,8 @@ export const skills = {
 
 export const projects = [
   {
-    title: " The insurance company Sure Tomorrow ML",
-    github: "https://github.com/kennyta20/The-insurance-company-Sure-Tomorrow",
+    title: "Net Zero Carbon Emissions",
+    github: "https://github.com/rishikesh2003/Prodigi",
     description: [
       "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
       "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
@@ -98,5 +98,57 @@ export const projects = [
       "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
       "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
     ],
+  },
+];
+
+export const awards = [
+  {
+    name: "IEEE YESIST12 Hackathon",
+    issuer: "IEEE",
+    date: "Sep 2022",
+    type: "International",
+    position: "Second Place",
+  },
+  {
+    name: "Prodigi Cognizant Hackathon",
+    issuer: "Cognizant",
+    date: "Feb 2023",
+    type: "National",
+    position: "Second Runner-up",
+  },
+  {
+    name: "Cisco Thingqbator Hackathon",
+    issuer: "Cisco",
+    date: "Jan 2023",
+    type: "National",
+    position: "First Runner-up",
+  },
+  {
+    name: "Innovators Day",
+    issuer: "Sri Manakula Vinayagar Engineering College, Pondicherry",
+    date: "Sep 2022",
+    type: "National",
+    position: "First Prize",
+  },
+  {
+    name: "KG Hackfest'22",
+    issuer: "KGiSL Institute of Technology, Coimbatore",
+    date: "Sep 2022",
+    type: "National",
+    position: "Second Prize",
+  },
+  {
+    name: "Innohacks'22",
+    issuer: "Innogeeks, KIET Group of Institutions, New Delhi",
+    date: "May 2022",
+    type: "National",
+    position: "Second Runner-up",
+  },
+  {
+    name: "Hack @ SKCET",
+    issuer: "Hackclub SKCET, SKCET, Coimbatore",
+    date: "Feb 2022",
+    type: "National",
+    position: "Most Impactful Hack",
   },
 ];
